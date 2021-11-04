2f6bed511d64ded815cc218ef7bb35f073aaee90
Gernot Bauer <bauer@itt.uni-stuttgart.de>
2021-11-04 19:21:11 +0100

    FAIL

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
 use pyo3::prelude::*;
+use numpy::{ToPyArray, PyReadonlyArray1};
 
 #[pyclass(name="FancyName")]
 pub struct MyClass {
     a: f64
+}
+
+/// This is my class
+#[pymethods]
+impl MyClass {
+    #[new]
+    pub fn new(a: PyReadonlyArray1<f64>) -> Self {
+        Self { a: 1.0 }
+    }
+
+   pub fn asd(&self) -> PyResult<()> { 
+       println!("{}", self.a);
+       Ok(())
+   }
 }
 
 #[cfg(feature="python_module")]
